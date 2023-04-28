bb7a74f9e887ae2314bf75638949afa6cbbdf7bf
Tien <ngotienpts@gmail.com>
2023-04-28 15:45:28 +0700

    28/4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade, Mousewheel, Pagination } from 'swiper';
 
-import { Champion, Welcome } from '../components/home-section';
+import { Champion, Welcome, ChampionDetail, Trailer, Credit } from '../components/home-section';
+import { championsData } from '../../src/assets/dummy';
 
 SwiperCore.use([EffectFade, Mousewheel, Pagination]);
 
@@ -23,7 +24,16 @@ const Home = () => {
             <Swiper {...swiperOptions}>
                 <SwiperSlide>{({ isActive }) => <Welcome isActive={isActive} />}</SwiperSlide>
                 <SwiperSlide>{({ isActive }) => <Champion isActive={isActive}></Champion>}</SwiperSlide>
+                <SwiperSlide>{({ isActive }) => <Trailer isActive={isActive}></Trailer>}</SwiperSlide>
+                <SwiperSlide>{({ isActive }) => <Credit isActive={isActive}></Credit>}</SwiperSlide>
             </Swiper>
+
+            {championsData.map((item, index) => {
+                return <ChampionDetail id={index} item={item} key={index} />;
+            })}
+            <div className="scroll">
+                <span>Scroll to see effect</span>
+            </div>
         </>
     );
 };
